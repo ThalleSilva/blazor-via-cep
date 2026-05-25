8eff65b6b3a24eb7056cefdad42cff78458bbdba
Thalles <167459991+ThalleSilva@users.noreply.github.com>
2026-05-24 22:49:56 -0300

    Add files via upload

---
+++ models/endereco.cs
@@ -0,0 +1,22 @@
+using System.Text.Json.Serialization;
+
+namespace BlazorViaCep.Models
+{
+    public class Endereco
+    {
+        [JsonPropertyName("cep")]
+        public string Cep { get; set; }
+
+        [JsonPropertyName("logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonPropertyName("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonPropertyName("localidade")]
+        public string Localidade { get; set; }
+
+        [JsonPropertyName("uf")]
+        public string Uf { get; set; }
+    }
+}
